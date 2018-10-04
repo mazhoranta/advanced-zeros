c8e3aad9f99b02debe60bf9af88f8d8bb26f58fe
mazhoranta <mazhoranta@gmail.com>
2018-10-04 23:58:44 +0300

    write function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const baseDivisors = [];
+  for (let i = 2; i <= base; i++) {
+    let pow = 0;
+    while (base % i == 0) {
+      pow++;
+      base /= i;
+    }
+
+    if (pow > 0) {
+      baseDivisors.push([i, pow]);
+    }
+  }
+  const zeros = [];
+  baseDivisors.forEach((divisor) => {
+    const prime = divisor[0],
+          pow = divisor[1];
+    let zerosCount = 0;
+    for (let i = prime; number / i >= 1; i *= prime) {
+      zerosCount += Math.floor(number / i);
+    }
+    zerosCount /= pow;
+    zeros.push(Math.floor(zerosCount));
+  });
+
+  return Math.min(...zeros);
 }
